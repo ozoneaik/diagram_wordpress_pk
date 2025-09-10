5d462a71344b6e82285f33b0910a7f4e07ed3db0
phuwadech <aofphuwadech@gmail.com>
2025-09-10 11:34:22 +0700

    add sweetalert2

--- resources/js/Pages/WordPress/WpDiagram.jsx
+++ resources/js/Pages/WordPress/WpDiagram.jsx
@@ -1,10 +1,22 @@
 import { Button } from "react-bootstrap";
+import Swal from "sweetalert2";
 
 
 export default function WpDiagram() {
+
+    const handleAlert = () => {
+        Swal.fire({
+            title: 'Error!',
+            text: 'Do you want to continue',
+            icon: 'error',
+            confirmButtonText: 'Cool'
+        });
+    }
     return (
         <>
-            <Button onClick={()=>alert('hello')} variant="primary">Primary</Button>
+            <Button onClick={handleAlert} variant="primary">
+                Primary
+            </Button>
         </>
     )
 }
