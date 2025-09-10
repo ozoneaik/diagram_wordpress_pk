e2f389d710104937ea71f07229d06f3f643656a1
phuwadech <aofphuwadech@gmail.com>
2025-09-10 10:29:11 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            ssr: 'resources/js/ssr.jsx',
+            refresh: true,
+        }),
+        react(),
+    ],
+    server: {
+        hmr: {
+            host: '029a6d85a932.ngrok-free.app',
+            protocol: 'wss',
+            clientPort: 443
+        },
+    },
+});
